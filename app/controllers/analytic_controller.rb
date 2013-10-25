f29780146d03d86810499a28f1ebeae77e329f17
Mohit <mknayak@ncsu.edu>
2013-10-25 16:07:56 -0400

    Code cleaning

--- app/controllers/analytic_controller.rb
+++ app/controllers/analytic_controller.rb
@@ -1,7 +1,6 @@
 class AnalyticController < ApplicationController
   include CourseHelper
   include AnalyticHelper
-
   before_filter :init
 
   def index
@@ -15,144 +14,83 @@ class AnalyticController < ApplicationController
     @available_courses = associated_courses(session[:user])
 
     #Hash of available method name of the data mining methods with different type of selection
-    @available_data_types = Hash.new
-    #data type by scope
-    @available_data_types[:course] = CourseAnalytic.instance_methods.map(&:to_s)
-    @available_data_types[:assignment] = AssignmentAnalytic.instance_methods.map(&:to_s)
-    @available_data_types[:team] = AssignmentTeamAnalytic.instance_methods.map(&:to_s)
-    #data type by chart type
-    @available_data_types[:bar] = [
-        #general
-        "num_participants",
-        "num_assignments",
-        "num_teams",
-        "num_participants",
-        "num_reviews",
-        #assignment_teams
-        "total_num_assignment_teams",
-        "average_num_assignment_teams",
-        "max_num_assignment_teams",
-        "min_num_assignment_teams",
-        #assignment_scores
-        "average_assignment_score",
-        "max_assignment_score",
-        "min_assignment_score",
-        #assignment_reviews
-        "total_num_assignment_reviews",
-        "average_num_assignment_reviews",
-        "max_num_assignment_reviews",
-        "min_num_assignment_reviews",
-        #team_reviews
-        "total_num_team_reviews",
-        "average_num_team_reviews",
-        "max_num_team_reviews",
-        "min_num_team_reviews",
-        #team_scores
-        "average_team_score",
-        "max_team_score",
-        "min_team_score",
-        #review_score
-        "average_review_score",
-        "max_review_score",
-        "min_review_score",
-        #review_word_count
-        "total_review_word_count",
-        "average_review_word_count",
-        "max_review_word_count",
-        "min_review_word_count",
-        #character_count
-        "total_review_character_count",
-        "average_review_character_count",
-        "max_review_character_count",
-        "min_review_character_count"
-    ]
+    @available_data_types= {:course => CourseAnalytic.instance_methods.map(&:to_s),
+                            :assignment => AssignmentAnalytic.instance_methods.map(&:to_s),
+                            :team => AssignmentTeamAnalytic.instance_methods.map(&:to_s)}
+    #Available statistics
+    @available_statistics = ['min', 'max', 'total', 'average']
 
-    @available_data_types[:scatter] = [
+    # Available attributes for each type of chart
+    @available_attributes = {:bar => ['num_assignment_teams',
+                                      'assignment_score',
+                                      'num_assignment_reviews',
+                                      'num_team_reviews',
+                                      'num_team_reviews',
+                                      'team_score',
+                                      'review_score',
+                                      'review_word_count',
+                                      'review_character_count'],
+                             :scatter => [],
+                             :line => [],
+                             :pie => []
+    }
 
-    ]
-    @available_data_types[:line] = []
-    @available_data_types[:pie] = []
+    # Create list of data_types for bar chart. Rest charts not implemented
+    @available_data_types[:bar] = @available_statistics.inject([]) do |result, statistic|
+      @available_attributes[:bar].each { |data_type| result.push("#{statistic}_#{data_type}") }
+      result
+    end
   end
 
   def graph_data_type_list
     #cross checking @available_data_type[chart_type] with @available_data_type[scope]
-    data_type_list = @available_data_types[params[:scope].to_sym] & @available_data_types[params[:type].to_sym]
-    #data_type_list.sort!
-    respond_to do |format|
-      format.json { render :json => data_type_list }
-    end
+    scope = params[:scope].to_sym
+    graph_type = params[:type].to_sym
+    data_type_list = @available_data_types[scope] & @available_data_types[graph_type]
+    data_type_list.sort!
+    render :json => data_type_list
   end
 
-  #dataPoint = [
-  #    {:name => 'review 1', :data => [9.9, 7.5, 6.4, 9.2, 4.0, 6.0, 5.6, 8.5, 6.4, 4.1, 5.6, 4.4]},
-  #    {:name => 'review 2', :data =>  [3.6, 8.8, 8.5, 3.4, 6.0, 4.5, 5.0, 4.3, 9.2, 8.5, 6.6, 9.3]},
-  #    {:name => 'review 3', :data => [8.9, 8.8, 9.3, 4.4, 7.0, 8.3, 9.0, 9.6, 5.4, 6.2, 9.3, 5.2]}
-  #]
-  #options2 = {
-  #    :title => "this is title2",
-  #    :x_axis_categories => [ 'Problem1', 'Problem2', 'Problem3', 'Problem4', 'Problem5', 'Problem6', 'Problem7', 'Problem8', 'Problem9', 'Problem10', 'Problem11','Problem12']
-  #}
-  #Chart.new(:bar, dataPoint, options2)
-  #should be rename to graph_data_packet
+  def incomplete_params?(params)
+    params[:id].nil? or params[:data_type].nil?
+  end
+
   def get_graph_data_bundle
-    puts params
-    if params[:id].nil? or params[:data_type].nil?
-      respond_to do |format|
-        format.json { render :json => nil }
-      end
-      return
-    end
+    a = Time.now
+    puts "entered get "
+    render :json => nil if incomplete_params?(params)
 
-    chart_method = {'line' => 'line_graph_data',
-                    'bar' => 'bar_chart_data',
-                    'scatter' => 'scatter_plot_data',
-                    'pie' => 'pie_graph_data' }
-    chart_data = send(chart_method[params[:type]], params[:scope], params[:id], params[:data_type])
+    scope = params[:scope]
+    graph_type = params[:type]
+    id = params[:id]
+    data_type = params[:data_type]
 
-    respond_to do |format|
-      format.json { render :json => chart_data }
-    end
+    chart_method_name = {'line' => 'line_graph_data',
+                         'bar' => 'bar_chart_data',
+                         'scatter' => 'scatter_plot_data',
+                         'pie' => 'pie_graph_data'}
+    chart_data = send(chart_method_name[graph_type], scope, id, data_type)
+    render :json => chart_data
+    puts "Exiting get #{Time.now - a} #{chart_data.to_json}"
   end
-
 
   def course_list
     courses = associated_courses(session[:user])
-    course_list = Array.new
-    courses.each do |course|
-      course_list << [course.name, course.id]
-    end
-    respond_to do |format|
-      format.json { render :json => sort_by_name(course_list) }
-    end
+    course_list = courses.map { |course| [course.name, course.id] }
+    render :json => sort_by_name(course_list)
   end
 
   def assignment_list
-    course = Course.find(params[:course_id])
+    course = Course.find(params[:course_id], :include => :assignments)
     assignments = course.assignments
-    assignment_list = Array.new
-    assignments.each do |assignment|
-      assignment_list << [assignment.name, assignment.id]
-    end
-    respond_to do |format|
-      format.json { render :json => sort_by_name(assignment_list) }
-    end
+    assignment_list = assignments.map { |assignment| [assignment.name, assignment.id] }
+    render :json => sort_by_name(assignment_list)
   end
 
   def team_list
     assignment = Assignment.find(params[:assignment_id])
-    teams = assignment.teams
-    team_list = Array.new
-    teams.each do |team|
-      team_list << [team.name, team.id]
-    end
-    respond_to do |format|
-      format.json { render :json => sort_by_name(team_list) }
-    end
+    teams_list = assignment.teams.map { |team| [team.name, team.id] }
+    render :json => sort_by_name(team_list)
   end
-
-  def render_sample
-
-  end
-
 
 end
