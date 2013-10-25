e4b1d8408891ad9fbe14cfa540709e3bff239472
Mohit <mknayak@ncsu.edu>
2013-10-25 18:04:38 -0400

    Reload classes in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Expertiza::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-
+  config.reload_classes_only_on_change = false
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 end
